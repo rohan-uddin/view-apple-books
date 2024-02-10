902b2289029f84457c76b09b32ab3106ea87c59d
Rohan Uddin <49825755+rohan-uddin@users.noreply.github.com>
2024-02-09 20:17:50 -0500

    it works - exports to local md files

--- app/api/highlights/route.tsx
+++ app/api/highlights/route.tsx
@@ -2,8 +2,17 @@ import { NextRequest, NextResponse } from "next/server";
 // import * as server from "@/components/providers/server";
 // import * as ibooks from "@/components/providers/ibooks";
 import ibooks from "@/components/providers/ibooks";
+const fs = require('fs');
+// const bodyParser = require('body-parser');
+const currentDate = new Date();
+
+interface HighlightData {
+  selectedText: string;
+  note: string;
+}
 
 /**
+ * FORMATTING FUNCTIONS ***********
  * This is what ibooks looks like:
  *
  * ibooks {
@@ -11,7 +20,6 @@ import ibooks from "@/components/providers/ibooks";
  *   getAnnotations: [AsyncFunction: getAnnotations]
  * }
  */
-console.log("ibooks", ibooks); 
 async function getBookTitles() {
   try {
     const bookData: any[] = [];
@@ -34,6 +42,54 @@ async function getBookTitles() {
   } catch (error) {
     console.error(error);
   }
+}
+
+function sendAnnotations(bookID: string, bookTitle: string, bookAuthor: string) {
+  ibooks.getAnnotations().then((value) => {
+    // console.log(value[0]);
+    // result gives you a filtered object with annotations specific to the book!
+    const result = value.filter((annot: { bookId: string }) => annot.bookId === bookID);
+
+    // console.log(result)
+
+    // Writing to a markdown file
+    let markdownOut = ""
+    markdownOut += `title::  ${bookTitle} (highlights)\n`
+    markdownOut += `author:: [[${bookAuthor}]]\n`
+    markdownOut += `full-title:: ${bookTitle}\n`
+    markdownOut += `category:: #books #Inbox \n`
+    markdownOut += `url:: \n`
+    markdownOut += `tags:: \n\n`
+    markdownOut += `- Highlights synced manually [[${formatDate(currentDate)}]]\n`
+
+
+    result.forEach((element:HighlightData) => {
+        markdownOut += `\t- ${element.selectedText}\n`
+        if (element.note) {markdownOut += `\t\t- ${element.note}\n`}
+    });
+
+    const path = 'exports'
+    if (fs.existsSync(path)) {
+      console.log('Folder exists!');
+      fs.writeFile(`exports/${bookTitle} (highlights).md`, markdownOut, (err: any) => {
+        if (err) throw err;
+        console.log("Data written to file");
+      });
+    } else {
+      console.log('Folder does not exist, making now...');
+      fs.mkdir('exports', (err: any) => {
+        if (err) {
+          console.error(err);
+        } else {
+          fs.writeFile(`exports/${bookTitle} (highlights).md`, markdownOut, (err: any) => {
+            if (err) throw err;
+            console.log("Data written to file");
+          });
+        }
+      });
+    }
+  });
+  return true
 }
 
 /**
@@ -59,7 +115,13 @@ export async function GET(request: NextRequest) {
 export async function POST(request: NextRequest) {
   try {
     const body = await request.json();
-    console.log("FROM THE SERVER: ", body)
+    // console.log("FROM THE SERVER: ", body)
+
+    for (const book of body) {
+      sendAnnotations(book.bookID, book.title, book.author);
+      console.log("FROM THE SERVER: just exported", book.title)
+    }
+
     return NextResponse.json({
       selected_books: body
     });
@@ -83,4 +145,21 @@ function formatBookJSON(rawTableData: { bookID: any; title: any; author: any; })
     author: rawTableData.author,
     checked: false,
   };
+}
+
+function formatDate(dateObj: Date): string {
+  const options: Intl.DateTimeFormatOptions = { year: "numeric", month: "long", day: "numeric" };
+
+  const formatter = new Intl.DateTimeFormat("en-US", options);
+  const formattedDate = formatter.format(dateObj).replace(/\b\d{1,2}\b/, match => match + getDaySuffix(Number(match)));
+
+  // console.log(formattedDate); // Output: June 22nd, 2022
+  return formattedDate;
+
+  function getDaySuffix(day: number): string {
+    const suffixes: string[] = ["th", "st", "nd", "rd"];
+    const relevantDigits = (day < 30) ? day % 20 : day % 30;
+    const suffix = (relevantDigits <= 3) ? suffixes[relevantDigits] : suffixes[0];
+    return suffix;
+  }
 }
